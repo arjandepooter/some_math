43ee8ba9f99ce7526c379810568e320552aa7f17
Arjan de Pooter <mail@arjandepooter.nl>
2021-06-12 22:32:19 +0200

    Remove main

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use some_math::polynomials::Polynomial;
-
-fn main() {
-    let p1 = Polynomial::new(vec![3.0, 4.6, 5.0]);
-    let p2 = Polynomial::new(vec![0.5, 0.6]);
-
-    println!("{}", p1 + p2 - Polynomial::zero());
-}
